b88cc89753819eccc22b800ed9f4c6b79d96975a
Dirane <diranengala@gmail.com>
2023-06-30 12:41:20 +0100

    Modified the swap digits

--- swap-digits-master/src/swapdigits.js
+++ swap-digits-master/src/swapdigits.js
@@ -1,11 +1,47 @@
-const swapDigits = (number)=>{
-    let swappedNumber = 0
-    //write logic here
-    if(number < 0 || number.length == 0){
-        swappedNumber = 0;
+
+const swapDigits = (number) => {
+    let swappedNumber = 0;
+    let digit1;
+    let digit2;
+    let count = 0;
+  
+    // Check if the number of digits is even
+    if (number.toString().length % 2 === 0) {
+      // Loop through the digits of the number and swap the alternate digits
+      while (number > 0) {
+        digit1 = number % 10;
+        number = Math.floor(number / 10);
+        digit2 = number % 10;
+        number = Math.floor(number / 10);
+  
+        // Add the swapped digits to the swapped number
+        swappedNumber += digit2 * Math.pow(10, count);
+        swappedNumber += digit1 * Math.pow(10, count + 1);
+  
+        count += 2;
+      }
+    } else {
+      // If the number of digits is odd, only swap the alternate digits starting from the second digit
+      while (number > 0) {
+        digit1 = number % 10;
+        number = Math.floor(number / 10);
+        digit2 = number % 10;
+        number = Math.floor(number / 10);
+  
+        // Add the swapped digits to the swapped number
+        if (count % 2 === 0) {
+          swappedNumber += digit2 * Math.pow(10, count);
+          swappedNumber += digit1 * Math.pow(10, count + 1);
+        } else {
+          swappedNumber += digit1 * Math.pow(10, count);
+          swappedNumber += digit2 * Math.pow(10, count + 1);
+        }
+  
+        count += 1;
+      }
     }
+  
     return swappedNumber;
-    
-}
+  };  
 
 module.exports = swapDigits
