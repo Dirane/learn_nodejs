a86b23af99c467279ed086f06fe039f72431a7fc
Dirane <diranengala@gmail.com>
2023-07-02 11:28:00 +0100

    Working with arrays

---
+++ Arrays_and_objects/js/script.js
@@ -0,0 +1,30 @@
+// let carMakes = ['Nissan', 'Ford', 'Tata'];
+
+// let carMakes = new Array("Nissan", "Ford", "Tata");
+
+// carMakes.push("BMW")
+
+// carMakes.unshift("Audi")
+
+// console.log(carMakes)
+
+// console.log("Removing elements");
+
+// carMakes.pop();
+
+// carMakes.shift();
+
+let cars = [];
+
+let make = 'agle';
+let model = 'Talon TSI';
+let color = 'Blue';
+
+let car = [];
+car.push(make);
+car.push(model);
+car.push(color);
+
+cars.push(car);
+
+console.log(cars)
