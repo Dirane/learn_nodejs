236979c086c03a63583643fdb3f15193359a6a6d
Dirane <diranengala@gmail.com>
2023-07-04 18:19:27 +0100

    Added logic to update cutomer and switch categories

--- banking-application-boilerplate-master/src/category.js
+++ banking-application-boilerplate-master/src/category.js
@@ -1,6 +1,19 @@
 var category = function category(key) {
   
+  key = key.toLowerCase();
   // Write the Logic here
+  switch(key) {
+    case 'pl':
+      return 'Personal Loan';
+    case 'vl':
+      return 'Vehicle Loan';
+    case 'el':
+      return 'Education Loan';
+    case 'hl':
+      return 'Home Loan';
+    default:
+      return 'Uncategorized';
+  }
 
   };
   
--- banking-application-boilerplate-master/src/customer.js
+++ banking-application-boilerplate-master/src/customer.js
@@ -9,16 +9,30 @@ const addCustomer=(CustomerId, CustomerName,CustomerAge,CustomerAddress,Customer
             CustomerAddress,
             CustomerContactNumber,
             Category
+      };
+
+      const existingCustomer = customerList.find((cust) => cust.CustomerId === CustomerId)
+      if(!existingCustomer){
+
+            customerList.push(customer);
       }
-      customerList.push(customer);
 }
 
 const updateCustomer=(CustomerId, CustomerName,CustomerAge,CustomerAddress,CustomerContactNumber,Category)=>{
       // Write the Logic here
+      const customer = customerList.find((cust)=> cust.CustomerId === CustomerId);
+      if(customer){
+            customer.CustomerName = CustomerName;
+            customer.CustomerAge = CustomerAge;
+            customer.CustomerAddress = CustomerAddress;
+            customer.CustomerContactNumber = CustomerContactNumber;
+            customer.Category = Category;
+      }
 }
 
 const getAllCustomers=()=>{
   // Write the Logic here
+  return customerList;
 }
 
 module.exports={addCustomer,updateCustomer,getAllCustomers}
