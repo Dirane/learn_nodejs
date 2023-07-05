7c7c90f7b513045aa279226997a5503706f1c14f
Dirane <diranengala@gmail.com>
2023-07-05 13:47:21 +0100

    Added the logic to do performance calculation

--- cricket-series-boilerplate-master/src/PerformanceCalc.js
+++ cricket-series-boilerplate-master/src/PerformanceCalc.js
@@ -1,7 +1,20 @@
-
-const PerformanceCalculator=(runs,balls)=>{
-      // Write the Logic here
-      
-}
-
-module.exports={PerformanceCalculator}
+const PerformanceCalculator = (runs, balls) => {
+      let average = runs / balls;
+    
+      if (runs > 30 && balls < 15) {
+        average += average * 0.01;
+      }
+    
+      if (runs > 50 && balls < 20) {
+        average += average * 0.1;
+      }
+    
+      if (runs > 100 && balls < 50) {
+        average += average * 0.2;
+      }
+    
+      return average;
+    };
+    
+    module.exports = { PerformanceCalculator };
+    
